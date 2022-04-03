068f826da6490f82be2c75246c695bda48424224
Sofichh <90448499+Sofichh@users.noreply.github.com>
2022-04-03 14:52:17 +0300

    Delete 3.py

--- 3.py
+++
@@ -1,9 +0,0 @@
-time = int(input('Время до обнуления: '))
-for i in range(time, -1, -1):
-    print('Секунд до взрыва', i)
-    ques = input('Хотите обезвредить бомбу сейчас ')
-    if ques == 'да':
-        print('Бомба обезврежена')
-        break
-else:
-    print('Бомба взорвалась')
