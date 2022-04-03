f80d7125aa607b81347d1ac3e23d5e2996e6df5e
Sofichh <90448499+Sofichh@users.noreply.github.com>
2022-04-03 14:49:18 +0300

    Delete 9.py

--- 9.py
+++
@@ -1,8 +0,0 @@
-x = int(input('Введите чило х: '))
-nominator = 1
-denominator = 1
-for i in range(1, 6):
-    nominator *= x - (2**i-1)
-    denominator *= x - 2**i
-res = nominator/denominator
-print('Вычет равен', res)
